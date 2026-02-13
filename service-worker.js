77c4d22d4d4a2c43fb58008b0a08ba6ef70b0df1
nagisa-ku <nagisa.ku.g@gmail.com>
2026-02-13 14:56:44 +0900

    reAdd
    いくつかのパスを修正

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'book-search-v1';
+const urlsToCache = [
+  '/bs-pwa/', // ルートパスを追加
+  '/bs-pwa/index.html', // BookSearch.htmlからindex.htmlに変更
+  '/bs-pwa/manifest.json',
+  '/bs-pwa/BaseData.csv',
+  '/bs-pwa/icon-192x192.png',
+  '/bs-pwa/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
